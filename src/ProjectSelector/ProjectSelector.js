676cef728b3013e31bd4b8fedd5c3078cb5ed791
alexjoyner <rosco9awj@gmail.com>
2020-08-25 09:17:54 -0400

    added ability to switch to a different project
    Signed-off-by: alexjoyner <rosco9awj@gmail.com>

--- src/ProjectSelector/ProjectSelector.js
+++ src/ProjectSelector/ProjectSelector.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { getProjects } from '../todoist_api_adapter';
-export const ProjectSelector = () => {
+export const ProjectSelector = ({ onSelect }) => {
 	const [projects, setProjects] = useState([]);
 
 	useEffect(() => {
-
+		async function fetchData() {
+			const response = await getProjects();
+			setProjects(response.projects);
+		}
+		fetchData();
 	}, []);
 
-	return [projects, {}];
+	return (
+		<div>
+			<h1>Projects</h1>
+			{projects.map(project => (
+				<div className="item" onClick={() => onSelect(project)}>
+					{project.name}
+				</div>
+			))}
+		</div>
+	);
 }
